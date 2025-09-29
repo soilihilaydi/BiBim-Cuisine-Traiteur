98a72213503bc9c93a012909cdb55e0f3f0025c8
H1m0t3p3 <h1m0t3p3@Mac-mini-de-H1m0t3p3.local>
2025-09-29 11:37:08 +0200

    changeemnt de couleur

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,9 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#FF6B35",
-        secondary: "#FFB870",
-        accent: "#FFD6A5",
+        primary: "#3A5A40",
+        secondary: "#A3B18A",
+        accent: "#F2E863",
       },
       fontFamily: {
         sans: ["Mochiy Pop One", "sans-serif"],
